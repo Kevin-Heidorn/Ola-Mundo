121f923e2659d963652fe75147bde5cce9aa9166
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2023-10-27 21:50:37 -0300

    evolução A2

--- site-exemplo/N.js
+++ site-exemplo/N.js
@@ -2,11 +2,9 @@
 const form = document.getElementById('loginForm');
 const loginButton = document.getElementById('loginButton');
 
-// Add an event listener for the button click
 loginButton.addEventListener('click', function () {
-    // Manually trigger the form submission
     form.submit();
 
-    // Redirect to another page after form submission
-    window.location.href = "https://way.animahub.com.br/#/novaInscricao/2023-2";
+  
+    
 });
